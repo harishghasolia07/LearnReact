0840f99b1e1dcd13f046b52a38394453e2cc94fa
harishghasolia07 <100846446+harishghasolia07@users.noreply.github.com>
2023-09-16 16:35:48 +0530

    Create Basic.js

---
+++ Basic.js
@@ -0,0 +1,2 @@
+// ReactDOM.render(What to show,Where to show)-
+// We can use the ReactDOM. render() in the application using the declaration of HTML code and the HTML element. The goal of this function is to represent the imposed HTML code within the specified HTML element tags. It helps to redirect the HTML page with the help of the render() function.
